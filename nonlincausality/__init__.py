9dfcfee489d579e834a69a08c90fb39bb84d497d
mrosol1 <mrosol5@gmail.com>
2020-09-05 23:10:14 +0200

    Commit

---
+++ nonlincausality/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  5 22:44:32 2020
+
+@author: MSc. Maciej Rosoł
+contact: mrosol5@gmail.com
+"""
+
+from .nonlincausality import(
+    nonlincausalityLSTM,
+    nonlincausalityGRU,
+    nonlincausalityNN,
+    nonlincausalityARIMAX,
+    nonlincausalitymeasureLSTM,
+    nonlincausalitymeasureGRU,
+    nonlincausalitymeasureNN,
+    nonlincausalitymeasureARIMAX
+    )
---
+++ nonlincausality/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  5 22:52:23 2020
+
+@author: MSc. Maciej Rosoł
+contact: mrosol5@gmail.com
+"""
+
+
+import setuptools
+
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Maciej Rosol",
+    author_email="mrosol5@gmail.com",
+    name='nonlincausality',
+    license="MIT",
+    description='Python package for Granger causality test with nonlinear forecasting methods.',
+    version='v1.0.0',
+    long_description=README,
+    url='https://github.com/mrosol/nonlincausality',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=['scipy', 'keras', 'statsmodels', 'tensorflow', 'matplotlib'],
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering'
+        'Topic :: Scientific/Engineering :: Information Analysis'
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ],
+)

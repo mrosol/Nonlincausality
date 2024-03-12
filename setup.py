f7a361131583ed512ac3566ba214fcc635efb65d
Maciej Rosoł <mrosol@MacBook-Pro-Maciej.local>
2024-03-12 18:00:32 +0100

    verison update

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     name='nonlincausality',
     license="MIT",
     description='Python package for Granger causality test with nonlinear (neural networks) forecasting methods.',
-    version='v2.0.0',
+    version='v2.0.2',
     long_description=README,
     long_description_content_type='text/markdown',
     url='https://github.com/mrosol/Nonlincausality',
